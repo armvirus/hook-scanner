0209d5bbad6e9df67be7132d55d445f5e7c408b5
armvirus <62994781+armvirus@users.noreply.github.com>
2023-11-05 21:48:31 +0100

    Add files via upload

---
+++ hook_scanner/include.hpp
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <cstdint>
+#include <string>
+#include <tlhelp32.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <Zydis/Zydis.h>
+
+#include "memory.hpp"
+#include "disassembler.hpp"
+#include "scanner.hpp"
